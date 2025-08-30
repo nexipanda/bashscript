fb2c9c7d03e2eb814e25145a7118e65a3cee8955
panpandev <victorvazsilveira@gmail.com>
2025-08-29 21:10:19 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-while true; do
-    echo "===== MENU ====="
-    echo "1) Mostrar data"
-    echo "2) Mostrar diretório atual"
-    echo "3) Listar arquivos"
-    echo "4) Mostrar usuários logados"
-    echo "5) Sair"
-    echo "================"
-    read -p "Escolha uma opção: " opcao
+Menu() {
+clear
+echo " Opções:"
+echo " 1."
+echo " 2."
+echo " 3."
+echo " A."
+echo " B."
+echo " C."
+echo
+echo -n "Esolha uma opção: "
+read OPTION
+echo
+case $OPTION in
+    1) echo "Você escolheu a opção 1" ;;
+    2) echo "Você escolheu a opção 2" ;;
+    3) echo "Você escolheu a opção 3" ;;
+    a|A|"") echo "Você escolheu a opção A" ;;
+    b|B|"") echo "Você escolheu a opção B" ;;
+    c|C|"") echo "Você escolheu a opção C" ;;
+    *) echo "Opção inválida" ;;
+esac
 
-    case $opcao in
-        1) date ;;
-        2) pwd ;;
-        3) ls ;;
-        4) who ;;
-        5) echo "Saindo..."; exit ;;
-        *) echo "Opção inválida!" ;;
-    esac
-
-    echo ""  # só para dar espaço
-done
+echo
+echo -n "Sair? [S/N] "
+read SAIR

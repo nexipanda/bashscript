d13ccf5e05eb0ecd2ffe82131026e2315e268c48
panpandev <victorvazsilveira@gmail.com>
2025-08-29 21:05:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-# menu.sh - Menu simples
 
-opcao=0
-until [ "$opcao" -eq 5 ]; do
-    echo "=============================="
-    echo "         MENU PRINCIPAL       "
-    echo "=============================="
-    echo "1) Mostrar data e hora"
+while true; do
+    echo "===== MENU ====="
+    echo "1) Mostrar data"
     echo "2) Mostrar diretório atual"
     echo "3) Listar arquivos"
     echo "4) Mostrar usuários logados"
     echo "5) Sair"
-    echo "------------------------------"
-    read -p "Escolha uma opção [1-5]: " opcao
+    echo "================"
+    read -p "Escolha uma opção: " opcao
 
     case $opcao in
         1) date ;;
         2) pwd ;;
-        3) ls -lah ;;
+        3) ls ;;
         4) who ;;
-        5) echo "Saindo..." ;;
-        *) echo "Opção inválida, tente novamente." ;;
+        5) echo "Saindo..."; exit ;;
+        *) echo "Opção inválida!" ;;
     esac
 
-    echo ""
+    echo ""  # só para dar espaço
 done

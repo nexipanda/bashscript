6d0a537dfcdbb1f0ee0480a1b239c1dc3f196999
panpandev <victorvazsilveira@gmail.com>
2025-08-30 14:32:01 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,9 @@ case $opcao in
     1)
         echo "web server instaler"
         sleep 2
-        curl -fsSL "https://raw.githubusercontent.com/nexipanda/bashscript/refs/heads/main/scripts/webserver.sh" | sudo bash
+        wget https://raw.githubusercontent.com/nexipanda/bashscript/refs/heads/main/scripts/webserver.sh
+        chmod u+x webserver.sh
+        ./webserver.sh
         ;;
     2)
         echo "Data atual:"

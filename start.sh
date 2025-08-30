3942d9ad6a74d1194d6ee45740632b209e378063
panpandev <victorvazsilveira@gmail.com>
2025-08-30 14:37:52 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,9 +16,13 @@ case $opcao in
     1)
         echo "web server instaler"
         sleep 2
-        wget https://raw.githubusercontent.com/nexipanda/bashscript/refs/heads/main/scripts/webserver.sh
-        chmod u+x webserver.sh
-        ./webserver.sh
+        # Baixa o script
+        wget -q https://raw.githubusercontent.com/nexipanda/bashscript/refs/heads/main/scripts/webserver.sh -O /tmp/webserver.sh
+        
+        # Torna executável e executa de forma interativa
+        chmod +x /tmp/webserver.sh
+        # Executa diretamente do terminal, não do pipe
+        bash /tmp/webserver.sh </dev/tty
         ;;
     2)
         echo "Data atual:"
